5bf4178acfd1428086f64b669ebb4dd17e14bf13
Henry Philpotts <hphilpotts@gmail.com>
2023-07-07 13:59:11 +0100

    Basic MUI AppBar added with logos.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import { CssBaseline } from '@mui/material'
+import Toolbar from '@mui/material/Toolbar'
+
+
+export const Navbar = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1, width: '100vw' }}>
+                <AppBar position="static">
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', bgcolor: "white" }}>
+                        <Box
+                            component="img"
+                            sx={{ maxHeight: { xs: 50, md: 100 }, margin: { xs: '10px', md: '20px' } }}
+                            alt="ECB logo. Dark blue: three lions passant under the crown"
+                            src="src/assets/ecbLogo.png"
+                        />
+                        <Box
+                            component="img"
+                            sx={{ maxHeight: { xs: 25, md: 75 }, margin: { xs: '10px', md: '20px' } }}
+                            alt="Silly Ashes logo. Mid green icon of ball hitting stumps next to 'Silly Ashes'."
+                            src="src/assets/sillashesLogo.png"
+                        />
+                        <Box
+                            component="img"
+                            sx={{ maxHeight: { xs: 50, md: 100 }, margin: { xs: '10px', md: '20px' } }}
+                            alt="Cricket Austraila emblem. Mid green shield with silver stars and a sun rising over the shadow of stumps."
+                            src="src/assets/ausLogo.png"
+                        />
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
+    );
+}
