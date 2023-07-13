92e512f1752b3e1e11b2df1e95c06370abe35b7d
Henry Philpotts <hphilpotts@gmail.com>
2023-07-13 11:13:04 +0100

    Updated AppBar: now narrower and with curved edges.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { AppBar, Box, TextField, Toolbar } from '@mui/material'
 
 export const Navbar = props => {
 
-    const calculatedWidth = props.containerWidth.width - 10 // 10 is the border
+    const calculatedWidth = props.containerWidth.width - 50
 
     console.log(calculatedWidth);
 
     return (
-            <AppBar position="fixed" sx={{ left: '50%', transform: 'translateX(-50%)', width: calculatedWidth }} >
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', bgcolor: "white" }}>
+            <AppBar position="fixed" sx={{ left: '50%', transform: 'translateX(-50%)', width: calculatedWidth, marginTop:'8px', borderRadius: '9px' }} >
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', bgcolor: "white", borderRadius: '8px' }}>
                     <Box
                         component="img"
                         sx={{ maxHeight: { xs: 25, md: 40 }, margin: '10px' }}
