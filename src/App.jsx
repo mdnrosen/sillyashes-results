e5e1e8a9df63ac25b8d59e5ada30ada536b8f3c9
Henry Philpotts <hphilpotts@gmail.com>
2023-07-13 11:21:52 +0100

    Tidied up Navbar.jsx code & additions to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from 'react'
+
 import { Container } from '@mui/material'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
@@ -6,16 +8,15 @@ import { Navbar } from './components/Navbar'
 
 import { Home } from './pages/Home'
 import { Player } from './pages/Player'
-import { useState, useRef, useEffect } from 'react'
 
 function App() {
 
   const containerRef = useRef()
-  const [containerWidth, setContainerWidth] = useState({ width: 0 })
+  const [containerWidth, setContainerWidth] = useState(0)
 
   const recalculateContainerWidth = () => {
     if(containerRef.current) {
-      setContainerWidth({ width: containerRef.current.offsetWidth})
+      setContainerWidth(containerRef.current.offsetWidth)
     }
   }
 
@@ -28,6 +29,7 @@ function App() {
   return (
     <Container className='app-container' id='app-container-id' ref={ containerRef } maxWidth='md' sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', paddingTop: { xs: '54px', md: '64px' } }}>
       <BrowserRouter>
+
         <Navbar containerWidth={containerWidth}/>
         <Routes>
           <Route path='/' element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,28 @@ import { AppBar, Box, TextField, Toolbar } from '@mui/material'
 
 export const Navbar = props => {
 
-    const calculatedWidth = props.containerWidth.width - 50
-
-    console.log(calculatedWidth);
+    const insideMarginPx = 25
+    const calculatedWidth = props.containerWidth - insideMarginPx * 2
 
     return (
-            <AppBar position="fixed" sx={{ left: '50%', transform: 'translateX(-50%)', width: calculatedWidth, marginTop:'8px', borderRadius: '9px' }} >
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', bgcolor: "white", borderRadius: '8px' }}>
-                    <Box
-                        component="img"
-                        sx={{ maxHeight: { xs: 25, md: 40 }, margin: '10px' }}
-                        alt="Silly Ashes logo. Mid green icon of ball hitting stumps next to 'Silly Ashes'."
-                        src="src/assets/sillashesLogo.png"
+        <AppBar position="fixed" sx={{ left: '50%', transform: 'translateX(-50%)', width: calculatedWidth, marginTop: '8px', borderRadius: '9px' }} >
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', bgcolor: "white", borderRadius: '8px' }}>
+                <Box
+                    component="img"
+                    sx={{ maxHeight: { xs: 25, md: 40 }, margin: '10px' }}
+                    alt="Silly Ashes logo. Mid green icon of ball hitting stumps next to 'Silly Ashes'."
+                    src="src/assets/sillashesLogo.png"
+                />
+                <Box>
+                    <TextField
+                        id="user-search"
+                        label="Search for name"
+                        type='search' variant='outlined'
+                        size='small' color=''
+                        sx={{ backgroundColor: 'white' }}
                     />
-                    <Box>
-                        <TextField id="user-search" label="Search for name" type='search' variant='outlined' size='small' color='' sx={{ backgroundColor: 'white' }}/>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
