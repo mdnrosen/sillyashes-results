8439c40db9f90d38531a1a4b7a2a5908de769a69
Henry Philpotts <hphilpotts@gmail.com>
2023-07-15 20:30:27 +0100

    Form with autocomplete added - not linked up to real data.

--- src/components/SearchToolbar.jsx
+++ src/components/SearchToolbar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 
 import data from '../assets/dummyData.json'
 
@@ -7,29 +7,37 @@ import SearchIcon from "@mui/icons-material/Search"
 
 
 export const SearchToolbar = () => {
-    // AppBar position can very easily be changed to 'sticky'
-    // - would need a bit of formatting if so: without a shadow/non-transparent bg it looks v odd 
+
+    const [playerSelected, setPlayerSelected] = useState({})
+
+    const submitHandler = e => {
+        e.preventDefault()
+        console.log('SUBMITTED:', playerSelected)
+    }
+
     return (
         <AppBar position='static' width={'100%'} sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
             <Toolbar display={'flex'} sx={{ width: { xs: '100%', md: 'auto' }, justifyContent: 'center' }}>
-                <Stack width={{ xs: 1, md: '50%' }} maxWidth={'370px'}>
-                    <Autocomplete
-                        id="user-search"
-                        freeSolo
-                        options={data.map((option) => option.name)}
-                        renderInput={(params) =>
-                            <TextField
-                                {...params}
-                                label="Search by name"
-                                placeholder='e.g. Jason Objects'
-                                color='success'
-                            />
-                        }
-                    />
-                </Stack>
-                <IconButton type='submit' aria-label='search'>
-                    <SearchIcon style={{ fill: "green" }} />
-                </IconButton>
+                <form style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                    <Stack width={{ xs: 1, md: '50%' }} maxWidth={'370px'}>
+                        <Autocomplete
+                            onChange={(e, value) => setPlayerSelected(value)}
+                            id="user-search"
+                            options={data.map((option) => option.name)}
+                            renderInput={(params) =>
+                                <TextField
+                                    {...params}
+                                    label="Search by name"
+                                    placeholder='e.g. Jason Objects'
+                                    color='success'
+                                />
+                            }
+                        />
+                    </Stack>
+                    <IconButton type='submit' aria-label='search' onClick={submitHandler}>
+                        <SearchIcon style={{ fill: "green" }} />
+                    </IconButton>
+                </form>
             </Toolbar>
         </AppBar>
     )
