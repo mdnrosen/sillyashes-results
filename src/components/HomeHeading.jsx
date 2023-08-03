0886ca7362485ad8642526f566806a1c3adf01b9
miles <mdnrosen@hotmail.co.uk>
2023-08-03 18:54:44 +0100

    fixed blur being too strong on mobile

--- src/components/HomeHeading.jsx
+++ src/components/HomeHeading.jsx
@@ -17,7 +17,7 @@ export const HomeHeading = ({ text }) => {
                         background: '-webkit-linear-gradient(right, #95ce39, #5fa92b, #337636, #4aa227)',
                         WebkitBackgroundClip: 'text',
                         WebkitTextFillColor: 'transparent',
-                        filter: { xs: 'blur(0.15px)', sm: '0.25px', md: '0.33px'}
+                        filter: { xs: 0, sm: '0.25px', md: '0.33px'}
                     }}
                 >
                     {text}
